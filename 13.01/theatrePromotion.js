89657bd147534fc91e01c89437c3ddc9777d6d28
MAD151 <mariaaleksandrova0204@gmail.com>
2021-01-16 14:22:41 +0000

    swith case
    I have used swith case

--- 13.01/theatrePromotion.js
+++ 13.01/theatrePromotion.js
@@ -3,35 +3,53 @@ function solve(param1, param2) {
     let day = String(param1);
     let age = Number(param2);
 
-    if (day = 'Weekday' && age >= 0 && age <= 18) {
-        console.log(`$${12}`);
-        if (day = 'Weekend' && age >= 0 && age <= 18){ 
-            console.log(`$${15}`);
-        }
-        if (day = 'Holiday' && age >= 0 && age <= 18) {
-            console.log(`$${5}`);
-        }
-    } else if (day = 'Weekday' && age < 18 && age <= 64) {
-        console.log(`$${18}`);
-        if (day = 'Weekend' && age < 18 && age <= 64) {
-            console.log(`$${20}`);
-        }
-        if (day = 'Holiday' && age < 18 && age <= 64) {
-        console.log(`$${12}`);
-        }
-    } 
-    else if (day = 'Weekday' && age < 64 && age <= 122) {
-        console.log(`$${12}`);
-        if (day = 'Weekend' && age < 64 && age <= 122) {
-            console.log(`$${15}`);
-        }
-        if (day = 'Holiday' && age < 64 && age <= 122) {
-        console.log(`$${10}`);
-        }
-    } 
-    else {
-        console.log("Error!")
+    switch (day) {
+        case 'Weekday':
+            if (age >=0 && age <= 18) {
+                console.log(`${12}$`);
+            }
+            else if (age > 18 && age <=64) {
+                console.log(`${18}$`);
+            }
+            else if (age > 64 && age <=122) {
+                console.log(`${12}$`);
+            }
+            else{
+                console.log('Error!');
+            }
+            break;
+        case 'Weekend':
+            if (age >=0 && age <= 18) {
+                console.log(`${15}$`);
+            }
+            else if (age >18 && age <=64) {
+                console.log(`${20}$`);
+            }
+            else if (age > 64 && age <=122) {
+                console.log(`${15}$`);
+            }
+            else{
+                console.log('Error!');
+            }
+            break;
+        case 'Holiday':
+            if (age >=0 && age <= 18) {
+                console.log(`${5}$`);
+            }
+            else if (age > 18 && age <=64) {
+                console.log(`${12}$`);
+            }
+            else if (age > 64 && age <=122) {
+                console.log(`${10}$`);
+            }
+            else{
+                console.log('Error!');
+            }
+            break;
+        default:
+            console.log('Error!');
+            break;
     }
+    
 }
-
 solve('Weekday', '42')
